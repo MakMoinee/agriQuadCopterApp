6e9566a103b2184fecd3d0a7b50c24cbfaf23589
Titus Silver <titus.silver.ph@gmail.com>
2025-03-23 01:50:40 +0800

    update code

--- app/src/main/java/com/drone/thesis/FullDroneActivity.java
+++ app/src/main/java/com/drone/thesis/FullDroneActivity.java
@@ -1,20 +1,74 @@
 package com.drone.thesis;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.drone.thesis.databinding.ActivityFullDroneBinding;
+import com.drone.thesis.models.Drones;
+import com.drone.thesis.services.DroneRequestService;
+import com.github.MakMoinee.library.dialogs.MyDialog;
+import com.github.MakMoinee.library.interfaces.LocalVolleyRequestListener;
+import com.google.gson.Gson;
 
 public class FullDroneActivity extends AppCompatActivity {
 
     ActivityFullDroneBinding binding;
+    DroneRequestService service;
+    Drones selectedDrone;
+    MyDialog myDialog;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityFullDroneBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        service = new DroneRequestService(FullDroneActivity.this);
+        selectedDrone = new Gson().fromJson(getIntent().getStringExtra("drones"), Drones.class);
+        if (selectedDrone != null) {
+            binding.txtDroneName.setText(selectedDrone.getDroneName());
+        }
+        myDialog = new MyDialog(FullDroneActivity.this);
+        myDialog.setCustomMessage("Sending Request ...");
+        setListeners();
+    }
+
+    private void setListeners() {
+        binding.btnTakeOff.setOnClickListener(v -> {
+
+            myDialog.show();
+            service.takeOff(selectedDrone.getIp(), new LocalVolleyRequestListener() {
+                @Override
+                public void onSuccessString(String response) {
+                    Toast.makeText(FullDroneActivity.this, "Successfully Send Request For Take Off", Toast.LENGTH_SHORT).show();
+                    myDialog.dismiss();
+                }
+
+                @Override
+                public void onError(Error error) {
+                    Toast.makeText(FullDroneActivity.this, "Failed To Send Request For Take Off, Please Try Again Later", Toast.LENGTH_SHORT).show();
+                    myDialog.dismiss();
+                }
+            });
+        });
+        binding.btnLanding.setOnClickListener(v -> {
+            myDialog.show();
+            service.landing(selectedDrone.getIp(), new LocalVolleyRequestListener() {
+                @Override
+                public void onSuccessString(String response) {
+                    Toast.makeText(FullDroneActivity.this, "Successfully Send Request To Land", Toast.LENGTH_SHORT).show();
+                    myDialog.dismiss();
+                }
+
+                @Override
+                public void onError(Error error) {
+                    Toast.makeText(FullDroneActivity.this, "Failed To Send Request To Land, Please Try Again Later", Toast.LENGTH_SHORT).show();
+                    myDialog.dismiss();
+                }
+            });
+        });
     }
 }
--- app/src/main/java/com/drone/thesis/services/DroneRequestService.java
+++ app/src/main/java/com/drone/thesis/services/DroneRequestService.java
@@ -6,6 +6,8 @@ import com.github.MakMoinee.library.interfaces.LocalVolleyRequestListener;
 import com.github.MakMoinee.library.models.LocalVolleyRequestBody;
 import com.github.MakMoinee.library.services.LocalVolleyRequest;
 
+import org.json.JSONObject;
+
 public class DroneRequestService extends LocalVolleyRequest {
 
     public DroneRequestService(Context mContext) {
@@ -14,5 +16,40 @@ public class DroneRequestService extends LocalVolleyRequest {
 
     public void pingDrone(LocalVolleyRequestBody body, LocalVolleyRequestListener listener) {
         this.sendJSONGetRequest(body, listener);
+    }
+
+
+    public void takeOff(String ip, LocalVolleyRequestListener listener) {
+        LocalVolleyRequestBody body = new LocalVolleyRequestBody.LocalVolleyRequestBodyBuilder()
+                .setUrl(String.format("http://%s/takeOff", ip))
+                .build();
+        this.sendJSONGetRequest(body, new LocalVolleyRequestListener() {
+            @Override
+            public void onSuccessJSON(JSONObject object) {
+                listener.onSuccessString("success");
+            }
+
+            @Override
+            public void onError(Error error) {
+                listener.onError(error);
+            }
+        });
+    }
+
+    public void landing(String ip, LocalVolleyRequestListener listener) {
+        LocalVolleyRequestBody body = new LocalVolleyRequestBody.LocalVolleyRequestBodyBuilder()
+                .setUrl(String.format("http://%s/land", ip))
+                .build();
+        this.sendJSONGetRequest(body, new LocalVolleyRequestListener() {
+            @Override
+            public void onSuccessString(String response) {
+                listener.onSuccessString("success");
+            }
+
+            @Override
+            public void onError(Error error) {
+                listener.onError(error);
+            }
+        });
     }
 }
